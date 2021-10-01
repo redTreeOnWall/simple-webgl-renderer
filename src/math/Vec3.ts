e99ff3c093750f29c80a1a9dd117cc0f1032fee4
redTree <1249776253@qq.com>
2021-10-01 16:33:12 +0800

    feat: vector3

--- src/math/Vec3.ts
+++ src/math/Vec3.ts
@@ -9,4 +9,22 @@ export class Vec3{
     this.y = y;
     this.z = z;
   }
+
+  static add (v1: Vec3, v2: Vec3, result: Vec3) {
+    result.set(
+      v1.x + v2.x,
+      v1.y + v2.y,
+      v1.z + v2.z,
+    );
+    return result;
+  }
+
+  static scale(v: Vec3, scale: number, result: Vec3) {
+    result.set(
+      v.x * scale,
+      v.y * scale,
+      v.z * scale,
+    );
+    return result;
+  }
 }
